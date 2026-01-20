4dfffb36a42966fdf1d8d25904145d78be95e43f
LuanCalais <luancalais@gmail.com>
2026-01-20 17:25:36 -0300

    feat: init users list component

--- mobile/lib/screens/users_list_screen.dart
+++ mobile/lib/screens/users_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:tasksphere_monitor/models/user.dart';
 import 'package:tasksphere_monitor/services/graphql_service.dart';
 
 class UsersListScreen extends StatefulWidget {
@@ -12,12 +14,41 @@ class _UsersListScreenState extends State<UsersListScreen> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    final client = GraphQLProvider.of(context).value;
+    svc = GraphqlService(client);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Usuários')),
-    );
+        appBar: AppBar(title: const Text('Usuários')),
+        body: FutureBuilder<List<User>>(
+            future: svc.fetchUsers(),
+            builder: (context, snap) {
+              if (snap.connectionState != ConnectionState.done)
+                return const Center(child: CircularProgressIndicator());
+              if (snap.hasError)
+                return Center(child: Text('Erro: ${snap.error}'));
+              final users = snap.data!;
+              return ListView.separated(
+                itemCount: users.length,
+                separatorBuilder: (_, __) => const Divider(),
+                itemBuilder: (context, i) {
+                  final u = users[i];
+                  return ListTile(
+                    leading: CircleAvatar(
+                        backgroundImage: u.profilePictureUrl != null
+                            ? NetworkImage(u.profilePictureUrl!)
+                            : null,
+                        child: u.profilePictureUrl == null
+                            ? Text(u.name[0])
+                            : null),
+                    title: Text(u.name),
+                    subtitle: Text(u.email),
+                    onTap: () => Navigator.pushNamed(context, '/'),
+                  );
+                },
+              );
+            }));
   }
 }
--- mobile/lib/services/graphql_service.dart
+++ mobile/lib/services/graphql_service.dart
@@ -1,11 +1,12 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:tasksphere_monitor/models/user.dart';
 
 class GraphqlService {
   final GraphQLClient client;
 
   GraphqlService(this.client);
 
-  static const String getUsersQuery = r'''
+  static const String GET_USERS = r'''
     query GetUsers {
       users(isActive: true) {
         id
@@ -16,4 +17,12 @@ class GraphqlService {
       }
     }
   ''';
+
+  Future<List<User>> fetchUsers() async {
+    final res = await client.query(QueryOptions(
+        document: gql(GET_USERS), fetchPolicy: FetchPolicy.networkOnly));
+    if (res.hasException) throw res.exception!;
+    final list = (res.data!['users'] as List).cast<Map<String, dynamic>>();
+    return list.map((json) => User.fromJson(json)).toList();
+  }
 }
